fc76b95212477ab105818df5963adac17185346a
xlanzh00 <128612888+xlanzh00@users.noreply.github.com>
2023-04-18 23:39:27 +0200

    Update default.py

--- default.py
+++ default.py
@@ -3,9 +3,9 @@ from Crypto.Util.strxor import strxor
 	"""
 	Ke komunikaci se serverem pouzijte nasledujici funkce:
 	
-	cbc.MAC_generation(text) - slouzi k vygenerovani zasifrovani textu
+	MAC_generation(text) - slouzi k vygenerovani zasifrovani textu
 	
-	cbc.MAC_authentication(tag) - slouzi k overeni spravnosti MAC tagu
+	MAC_authentication(tag) - slouzi k overeni spravnosti MAC tagu
 	
 	"""
 
