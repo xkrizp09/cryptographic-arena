f43d1341a0e81bd6f84f8bc9b611b8df089b2d8b
xlanzh00 <128612888+xlanzh00@users.noreply.github.com>
2023-04-18 23:40:16 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-import cbc
+from cbc import *
 from Crypto.Util.strxor import strxor
 
 def exploit():
@@ -6,12 +6,12 @@ def exploit():
 	target_string = "Stesti preje vsem odvaznym lidem"
 	#Rozdeleni stringu na bloky o velikosti 16b a sifrovani
 	first_slice = target_string[:16]
-	MAC_first_slice = bytes.fromhex(cbc.MAC_generation(first_slice))
+	MAC_first_slice = bytes.fromhex(MAC_generation(first_slice))
 	#Vypocet druhe poloviny v zavislosti na prvnim sifrovanem bloku pomoci XOR
 	second_slice = strxor(MAC_first_slice,target_string[16:32].encode())
-	MAC = cbc.MAC_generation(second_slice)
+	MAC = MAC_generation(second_slice)
 	#Overeni vysledneho MAC tagu
-	cbc.MAC_authentication(MAC)
+	MAC_authentication(MAC)
 if __name__ == '__main__':
 	exploit()
 
