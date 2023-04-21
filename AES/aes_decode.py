2d9d3a2c98836b10286c9328c36c957b6cb560f7
petrK98 <104077208+petrK98@users.noreply.github.com>
2023-04-22 00:31:13 +0200

    Update aes_decode.py
    Code description

--- AES/aes_decode.py
+++ AES/aes_decode.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
+#The binascii function is used to convert the decoded sequence of bytes into a text string.
 import binascii
 from operator import xor
 
+#Opening a file called "output2.txt". The coded text from which the given flag is obtained is stored here.
 with open("output2.txt") as h:
-  
-#Complete the remaining code here
 
+#Complete the remaining code here.
+
+
+#The flag listing.
 print(f"{flag=}")
