314f330660e1e2ed5005b877945e9e4e08dd70c7
kriz.pe22@gmail.com <kriz.pe22@gmail.com>
2023-04-21 16:05:04 +0200

    add helper functions

--- solutions/extractSolution.py
+++ solutions/extractSolution.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 import sys
      
 def showData(image):
@@ -13,7 +12,7 @@ def showData(image):
     for row in image:
         for pixel in row:
             #convert the red, green and blue values into binary format
-            r, g, b = messageToBinary(pixel) 
+            r, g, b = helperFunctions.messageToBinary(pixel) 
             
             #extracting data from the least significant bit of red pixel
             binary_data += r[-1]
@@ -25,7 +24,7 @@ def showData(image):
             binary_data += b[-1]
           
     # split by 8-bits
-    all_bytes = splitBinaryData(binary_data)
+    all_bytes = helperFunctions.splitBinaryData(binary_data)
   
     # convert from bits to characters from all_bytes
     for byte in all_bytes:
@@ -37,32 +36,7 @@ def showData(image):
         
     #remove the delimeter to show the original hidden message  
     return decoded_data[:-3]
-
-def messageToBinary(message):  # define a function called "messageToBinary" that takes a single argument "message"
-    if type(message) == str:  # check if "message" is a string 
-        return ''.join([ format(ord(i), "08b") for i in message ])# convert each character of the string to its binary representation using the "ord" function and format it as an 8-bit string with leading zeros ("08b"); join the resulting binary strings together and return as a single string
-    elif type(message) == bytes or type(message) == np.ndarray:  # check if "message" is a bytes or numpy array object
-        return [ format(i, "08b") for i in message ] # apply binary formatting to each element of the array using a list comprehension and return a list of binary strings
-    elif type(message) == int or type(message) == np.uint8:  # check if "message" is an integer or numpy uint8 object
-        return format(message, "08b") # format the integer directly as an 8-bit binary string and return it
-    else:  # if "message" is not one of the supported types, raise a TypeError with a custom error message
-        raise TypeError("Vstup neni podporovan")
-
-# Split a binary string into 8-bit chunks
-def splitBinaryData(binary_data):
-    # Define a list to store the 8-bit chunks
-    chunks = []
-    
-    # Iterate over the binary string with a step size of 8
-    for i in range(0, len(binary_data), 8):
-        
-        # Extract an 8-bit chunk starting from the current index and append it to the list
-        chunk = binary_data[i: i+8]
-        chunks.append(chunk)
-        
-    # Return the list of 8-bit chunks
-    return chunks
-                
+     
 def main():
     if len(sys.argv) == 2:
         image = sys.argv[1]
--- steganography/extract.py
+++ steganography/extract.py
@@ -1,8 +1,9 @@
 import cv2
-import numpy as np
 import sys
+import helperFunctions
+
      
-def showData(image):
+def extractData(image):
     # Load the image
     image = cv2.imread(image) 
     
@@ -22,7 +23,7 @@ def showData(image):
             #extracting data from the least significant bit of red pixel
           
     # split by 8-bits
-    all_bytes = splitBinaryData(binary_data)
+    all_bytes = helperFunctions.splitBinaryData(binary_data)
   
     for byte in all_bytes:
         # convert from bits to characters from all_bytes
@@ -33,32 +34,11 @@ def showData(image):
        
     #remove the delimeter to show the original hidden message  
     return decoded_data[:-3]
-
-# define a function called "messageToBinary" that takes a single argument "message"
-def messageToBinary(message):  # define a function called "messageToBinary" that takes a single argument "message"
-    if type(message) == str:  # check if "message" is a string 
-        return ''.join([ format(ord(i), "08b") for i in message ])# convert each character of the string to its binary representation using the "ord" function and format it as an 8-bit string with leading zeros ("08b"); join the resulting binary strings together and return as a single string
-    elif type(message) == bytes or type(message) == np.ndarray:  # check if "message" is a bytes or numpy array object
-        return [ format(i, "08b") for i in message ] # apply binary formatting to each element of the array using a list comprehension and return a list of binary strings
-    elif type(message) == int or type(message) == np.uint8:  # check if "message" is an integer or numpy uint8 object
-        return format(message, "08b") # format the integer directly as an 8-bit binary string and return it
-    else:  # if "message" is not one of the supported types, raise a TypeError with a custom error message
-      raise TypeError("Vstup neni podporovan")
-
-# Split a binary string into 8-bit chunks
-def splitBinaryData(binary_data):
-    chunks = [] # Define a list to store the 8-bit chunks
-    
-    for i in range(0, len(binary_data), 8): # Iterate over the binary string with a step size of 8
-        chunk = binary_data[i: i+8] # Extract an 8-bit chunk starting from the current index and append it to the list
-        chunks.append(chunk)
-        
-    return chunks # Return the list of 8-bit chunks
-                
+              
 def main():
     if len(sys.argv) == 2:
         image = sys.argv[1]
-        print("Tajná zprava je: " + showData(image)) 
+        print("Tajná zprava je: " + extractData(image)) 
     else:
         print("Chyba: Musíte zadat jeden parametr.")
 
