94b00bd2a36b9a298add89ff2e8cc0b2ba3a99bb
xlanzh00 <128612888+xlanzh00@users.noreply.github.com>
2023-04-21 13:58:12 +0200

    Translation

---
+++ cbc-mac/default.py
@@ -0,0 +1,19 @@
+from cbc import *
+from Crypto.Util.strxor import strxor
+"""
+To communicate with server use the following functions:
+	
+MAC_generation(text) - used to cipher block of the message
+	
+MAC_authentication(tag) - used for checking if generated tag is correct
+	
+"""
+
+
+def exploit():
+	
+	
+	
+	
+if __name__ == '__main__':
+	exploit()
