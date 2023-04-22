51c180e9a54d048be2f5ce7f383ba57dda9c0fc2
petrK98 <104077208+petrK98@users.noreply.github.com>
2023-04-22 13:41:59 +0200

    Update aesDecode.py

--- aes/aesDecode.py
+++ aes/aesDecode.py
@@ -6,9 +6,15 @@ from operator import xor
 
 #Opening a file called "output2.txt". The coded text from which the given flag is obtained is stored here.
 with open("output2.txt") as h:
-
 #Complete the remaining code here.
 
+#store the content into two bytearrays
+
+#create blob bytearray for XOR operation between these two bytearrays
+
+test = b"No right of private conversation was enumerated in the Constitution. I don't suppose it occurred to anyone at the time that it could be prevented."
+
+#Create a bytearray as the result of an XOR operation between the blob and the first len(xxx) bytes of the test string.
 
 #The flag listing.
 print(f"{flag=}")
