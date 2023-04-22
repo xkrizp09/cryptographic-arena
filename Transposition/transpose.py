f18a0a0a3f4151b30fe7871daf13df71b6787433
petrK98 <104077208+petrK98@users.noreply.github.com>
2023-04-22 09:50:35 +0200

    transpose.py
    Complete the remaining part of the code.

---
+++ Transposition/transpose.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#It opens the message.txt file for reading and reads the contents of the file and stores it in the contents variable.
+with open("message.txt") as filp:
+    contents = filp.read()
+
+    
+#Complete the remaining part of the code.
+
+
+#Prints the flag variable.
+print(flag)
