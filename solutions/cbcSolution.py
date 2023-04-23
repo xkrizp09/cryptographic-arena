0e16d3f98b5af0cf16ed19058518fc1bb7bd7565
xlanzh00 <128612888+xlanzh00@users.noreply.github.com>
2023-04-23 12:29:13 +0200

    Update cbcSolution.py

--- solutions/cbcSolution.py
+++ solutions/cbcSolution.py
@@ -1,10 +1,17 @@
 from Crypto.Util.strxor import strxor
 import importlib.util
-
 spec = importlib.util.spec_from_file_location("cbc", ".cbc.py")
 cbc = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(cbc)
 
+"""
+To communicate with server use the following functions:
+	
+cbc.MAC_generation(text) - used to cipher block of the message
+	
+cbc.MAC_authentication(tag) - used for checking if generated tag is correct
+	
+"""
 def exploit():
 	#Text we want to create MAC from
 	target_string = "CBC-MAC is really interesting!!!"
