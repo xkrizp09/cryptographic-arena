62a8e2c081f1f6218d2f73779d38faea501d853d
Luisiki <xkuner00@vutbr.cz>
2023-04-23 16:01:57 +0200

    Added code for testing

---
+++ test/hash/Hash.py
@@ -0,0 +1,31 @@
+import hashlib
+
+#implement a funtion which will read the password file and will parse the code into sigle list elements.
+def readPasswords():
+   #todo implement
+   return passwords
+
+
+#creates hashes of the passwords and returns the resulting hashes
+def getHashes(passwords):
+    result = []
+    for i in passwords:
+        hash = hashlib.md5(i.encode())
+        result.append(hash.hexdigest())
+    return result
+
+#implement a function which will find the collision and will return the index of the position where to collision was found
+def findCollision(hashes, passhash):
+    #todo implement
+    return index
+        
+
+
+
+
+passwords = readPasswords() #implement
+hashes = getHashes(passwords)
+
+
+number = findCollision(hashes, "input hash")#implement
+print("Password is :" , passwords[number])
---
+++ test/lenstra/Lenstra.py
@@ -0,0 +1,108 @@
+import random
+from numpy import sqrt
+
+
+def gcd(a, b):
+    return a if (b==0) else gcd(b, a % b)
+
+#a^n mod c
+def PowMod(a, n, c):
+    ans = 1
+    base = a % c
+    while n:
+        if n & 1:
+            ans = (ans * base) % c
+        base = (base * base) % c
+        n //= 2
+    return ans
+
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+#P + P definition
+def pointAddition(point, curve):
+    result = Point
+
+    #implement lambda calculation with the point coordinates calculation
+
+    return result
+
+class EllipticCurve:
+    def __init__(self, a, b, modulo):
+        self.a = a
+        self.b = b
+        self.modulo = modulo
+
+
+def LenstraEllipticCurveFactorization(modulus):
+    if modulus % 2 == 0:
+        return 2
+    elif modulus % 3 == 0:
+        return 3
+    elif modulus % 5 == 0:
+        return 5
+    elif modulus % 7 == 0:
+        return 7
+    elif modulus % 11 == 0:
+        return 11
+    #Elementary divisor elimination
+
+    
+    P0 = Point
+    P0.x = random.randint(1, modulus - 1)
+    P0.y = random.randint(1, modulus - 1)
+    #random point generation
+    
+    E = EllipticCurve
+    E.a = random.randint(1, modulus - 1)
+    E.b = random.randint(1, modulus - 1)
+    E.modulo = modulus
+    #random curve
+
+
+    points = [P0]
+    d = 0
+
+    for i in range(1, (int)(sqrt(modulus))):
+
+        #implement point addition
+        points.append(pointAddition(points[i-1], E))
+
+
+        d = (int)(gcd((int)(modulus), (int)(points[i-1].y)))
+
+        #test if the divisor has been found
+        if(d > 1 & d < modulus):
+            return d
+
+
+    return -1
+
+
+#executes the Factorization and prints out the found divisors and returns a list with the found divisors
+def LECF(modulus):
+    results = []
+    while (modulus != 1):
+        results.append(LenstraEllipticCurveFactorization(modulus))
+        if(results[-1] == -1):
+            results[-1] = (int)(modulus)
+            print(results[-1])
+            return results
+
+        modulus/= results[-1]
+        print(results[-1])
+
+
+
+
+
+
+
+
+#this will print the factors, input is the number to be factored
+LECF()  
