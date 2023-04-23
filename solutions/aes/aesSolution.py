ff5d3d5917ba8f5ff03df89f3df988e52f12c50a
petrK98 <104077208+petrK98@users.noreply.github.com>
2023-04-23 22:23:10 +0200

    Update aesSolution.py

--- solutions/aes/aesSolution.py
+++ solutions/aes/aesSolution.py
@@ -1,17 +1,18 @@
-#Imports the binascii module and the xor function from the operator module. The binascii function contains functions to convert strings to and from various formats, including hexadecimal representation.
+#Imports the binascii module and the xor function from the pwn tools. The binascii function contains functions to convert strings to and from various formats, including hexadecimal representation.
 import binascii
-from operator import xor
+from pwn import xor
 
-#Opens the output2.txt file for reading, reads the first and second line of the file and converts the hexadecimal representation to a bytearray.
+#Opens the output2.txt file for reading, reads the first and second line of the file and convert the hexadecimal-encoded strings to their corresponding bytes representation.
 with open("output2.txt") as h:
-    encrypted_test = bytearray.fromhex(h.readline().strip())
-    encrypted_flag = bytearray.fromhex(h.readline().strip())
+    encrypted_test = binascii.unhexlify(h.readline().strip())
+    encrypted_flag = binascii.unhexlify(h.readline().strip())
     
-#Creates a bytearray named blob as the result of the XOR operation between encrypted_test and encrypted_flag.
-blob = bytearray(a ^ b for a, b in zip(encrypted_test, encrypted_flag))
+#Creates an object named blob as the result of the XOR operation between encrypted_test and encrypted_flag.
+blob = xor(encrypted_test, encrypted_flag)
 
 test = b"No right of private conversation was enumerated in the Constitution. I don't suppose it occurred to anyone at the time that it could be prevented."
 
-#Creates a bytearray named "flag" as the result of an XOR operation between the blob and the first len(encrypted_flag) bytes of the test string.
-flag = bytearray(a ^ b for a, b in zip(blob, test[: len(encrypted_flag)]))
-print(f"{flag=}")
+#Creates an object named "flag" as the result of an XOR operation between the blob and the first len(encrypted_flag) bytes of the test string.
+flag = xor(blob, test[: len(encrypted_flag)]) [: len(encrypted_flag)]
+FLAG = flag.decode('utf-8')[5:-1]
+print(f"FLAG=({FLAG})")
