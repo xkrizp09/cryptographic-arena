a4c5d60fbbc7a4f56c0e9a6d416b03d61224deb3
petrK98 <104077208+petrK98@users.noreply.github.com>
2023-04-23 22:31:18 +0200

    Update aesDecode.py

--- aes/aesDecode.py
+++ aes/aesDecode.py
@@ -2,19 +2,21 @@
 
 #The binascii function is used to convert the decoded sequence of bytes into a text string.
 import binascii
-from operator import xor
+from pwn import xor
 
 #Opening a file called "output2.txt". The coded text from which the given flag is obtained is stored here.
 with open("output2.txt") as h:
 #Complete the remaining code here.
 
-#store the content into two bytearrays
+#store the content into two binasciiarrays
 
-#create blob bytearray for XOR operation between these two bytearrays
+#create an object blob for XOR operation between these two binasciiarrays
 
 #Define a variable that contains a bytes object for testing.
 
-#Create a bytearray as the result of an XOR operation between the blob and the first len(xxx) bytes of the test string.
+#Create an object as the result of an XOR operation between the blob and the first len(xxx) bytes of the test string.
 
 #The flag listing.
-print(f"{flag=}")
+
+FLAG = flag.decode('utf-8')[5:-1]
+print(f"FLAG({FLAG})")
